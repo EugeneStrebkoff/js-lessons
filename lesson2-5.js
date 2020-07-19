9fa34318f0bdefd060e8b7909715afd751f9ff29
Eugene Strebkov <evgeny.strebkoff@gmail.com>
2020-07-19 20:26:22 +0300

    Добавлено решение задания №5

---
+++ lesson2-5.js
@@ -0,0 +1,22 @@
+//Задание №5
+
+function summ(a, b) {
+    return  a + b
+}
+
+function subtraction(a, b) {
+    return a - b
+}
+
+function multiplication(a, b) {
+    return a * b
+}
+
+function division(a, b) {
+    return a / b
+}
+
+alert(summ(2, 2))
+alert(subtraction(5, 2))
+alert(multiplication(4, 4))
+alert(division(33, 3))
