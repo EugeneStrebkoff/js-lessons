91f7e2d63d2e4dd4cb7cb95ebde26eb48accab82
Eugene Strebkov <evgeny.strebkoff@gmail.com>
2020-07-23 09:57:51 +0300

    Добавлено решение задач №2 и №3

---
+++ js/lesson3-2.js
@@ -0,0 +1,14 @@
+let iphone = 999;
+let samsung = 888;
+let xiaomi = 100;
+let siemens = 13;
+let basket = [iphone, samsung, xiaomi, siemens];
+
+function countBasketPrice(box) {
+    box = basket;
+    let sum = 0;
+    for (let i = 0; i < box.length; i++){
+        sum += box[i];
+    }
+    return sum;
+}
