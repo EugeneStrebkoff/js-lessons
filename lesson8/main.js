b2bbd73f2951d7a2af938f8bc28e8d5c45d62eba
Eugene Strebkov <evgeny.strebkoff@gmail.com>
2020-08-15 17:24:35 +0300

    Добавлен основной функционал магазина

--- lesson8/main.js
+++ lesson8/main.js
@@ -26,7 +26,11 @@ class Product {
         btn.classList.add('btn-buy');
         btn.innerText = 'Купить';
         btn.addEventListener('click', () => {
+            const CartInstance = new Cart();
             CartInstance.add(this);
+            let cartBlock = document.querySelector('.cart-block');
+            cartBlock.innerText = '';
+            CartInstance.render();
         })
         return btn;
     }
@@ -48,7 +52,6 @@ class List {
     }
     add(product) {
         this.items.push(product);
-        this.render();
     }
 
     del() {
@@ -59,49 +62,97 @@ class List {
 }
 
 class Cart extends List {
-    constructor(){
-        if(Cart._instance){
+    sumOfPrice = 0;
+    sumOfGoods = 0;
+    constructor() {
+        if (Cart._instance) {
             return Cart._instance;
         }
         super();
-
+        this.mainCartTemplate();
         Cart._instance = this;
     }
-    shown = false;
-    render() {
+
+    mainCartTemplate() {
         let header = document.querySelector('.header');
         let block = document.createElement('button');
         block.classList.add('cart-btn');
         block.innerText = 'Корзина';
         let cartDiv = document.createElement('div');
         cartDiv.classList.add('cart-block');
-        if (!this.items.length){
-            cartDiv.innerText = 'Корзина пока что пуста'
-        } else {
-            this.items.forEach(elem => function(){
-                let itemBlock = document.createElement('div');
-                itemBlock.innerText = `Товар: ${elem.name} по цене ${elem.price}$ в количестве ${elem.count} = ${elem.price * elem.count}$`;
-                cartDiv.appendChild(itemBlock);
-            })
-        }
+        cartDiv.innerText = 'Корзина пока что пуста';
         header.appendChild(block);
         header.appendChild(cartDiv);
         this.btn();
     }
 
+    render(){
+        let cartDiv = document.querySelector('.cart-block');
+        this.items.forEach(elem => {
+            let itemBlock = document.createElement('div');
+            itemBlock.innerHTML = `Товар: ${elem.name} по цене ${elem.price}$ в количестве ${elem.count} = ${elem.price * elem.count}$`;
+            let btnInc = document.createElement('button');
+            btnInc.classList.add('btn-inc');
+            btnInc.innerText = '+';
+            btnInc.addEventListener('click', () =>{
+                elem.inc();
+                this.sumOfPrice += elem.price;
+                this.sumOfGoods++;
+                cartDiv.innerHTML = '';
+                this.render();
+        
+        })
+            let btnDec = document.createElement('button');
+            btnInc.classList.add('btn-dec');
+            btnDec.innerText = '-';
+            btnDec.addEventListener('click', () =>{
+                elem.dec();
+                this.sumOfPrice -= elem.price;
+                this.sumOfGoods--;
+                cartDiv.innerHTML = '';
+                if(elem.count === 0){
+                    this.items.splice(this.items.indexOf(elem), 1);
+                    elem.count = 1;
+                }
+                this.render();
+        })
+            cartDiv.appendChild(itemBlock);
+            cartDiv.appendChild(btnInc);
+            cartDiv.appendChild(btnDec);
+        })
+
+        let sumBlock = document.createElement('div');
+        if(this.sumOfPrice === 0){
+            sumBlock.innerText = 'Корзина пока что пуста';
+        } else {
+            sumBlock.innerText = `Всего в корзине ${this.sumOfGoods} товара(ов) на сумму ${this.sumOfPrice}`;
+        }
+        cartDiv.appendChild(sumBlock);
+    }
+
+    productInstance(product){
+        if(this.items.indexOf(product) != -1){
+            product.inc();
+        } else {
+            this.items.push(product);
+        }
+        this.sumOfPrice += product.price;
+        this.sumOfGoods++;
+    }
+
+    add(product){
+        this.productInstance(product);
+    }
+
     btn() {
         let block = document.querySelector('.cart-btn');
         block.addEventListener('click', () => {
-            let cartDiv = document.querySelector('.cart-block');
-            if (!this.shown) {
-                cartDiv.classList.add('shown');
-                this.shown = true;
-            } else {
-                cartDiv.classList.remove('shown');
-                this.shown = false;
-            }
-
+            let cart = document.querySelector('.cart-block');
+            cart.classList.toggle('shown');
         })
+    }
+
+    btnInc(){
     }
 }
 
@@ -117,13 +168,12 @@ class Catalog extends List {
     }
 }
 
-const catalogList = new Catalog();
+const CatalogList = new Catalog();
 
-catalogList.add(new Product('1', 1));
-catalogList.add(new Product('2', 2));
-catalogList.add(new Product('3', 3));
+CatalogList.add(new Product('1', 1));
+CatalogList.add(new Product('2', 2));
+CatalogList.add(new Product('3', 3));
 
-catalogList.render();
+CatalogList.render();
 
 const CartInstance = new Cart();
-CartInstance.render();
